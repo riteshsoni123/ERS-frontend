618c37db0c19e972c8da51660cbeb31ec94898a1
riteshsoni123 <comedybuddy1@gmail.com>
2021-07-31 19:56:58 +0530

    Added and styled the about us page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Navi from "./components/Navbar/Navi";
-import Landing from "./components/Landing/Landing";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
         <Navi/>
-        <Landing/>
+        <About/>
       hello
     </div>
   );
