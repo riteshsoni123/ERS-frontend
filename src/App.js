b9295297305345b72d4b1fd55daf025c7b4a4720
masterchief164 <63920595+masterchief164@users.noreply.github.com>
2021-08-01 01:37:50 +0530

    main landing page styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Navi from "./components/Navbar/Navi";
-import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
         <Navi/>
-        <About/>
       hello
     </div>
   );
