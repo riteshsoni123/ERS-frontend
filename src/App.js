aad1878e5fefc79ce1e4b37e4a7e8e0036971ee2
riteshsoni123 <comedybuddy1@gmail.com>
2021-08-05 18:58:30 +0530

    added the structure for events page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from "./components/About/About";
 import Landing from "./components/Landing/Landing";
 import Team from "./components/Team/Team";
 import Map from "./components/Map/Map";
+import Events from "./components/Events/Events";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Navi/>
         <Landing/>
         <About/>
+        <Events/>
         <Team/>
         <Map/>
     </div>
---
+++ src/components/Events/Events.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Events.scss";
+
+const Events = () => {
+    return (
+        <section>
+            <div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Events;
