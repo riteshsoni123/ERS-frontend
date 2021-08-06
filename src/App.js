8ef837dbde270330b4f5dcc35ad51c0ba899a3ce
riteshsoni123 <comedybuddy1@gmail.com>
2021-08-06 13:51:05 +0530

    solved the conflict in app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import About from "./components/About/About";
 import Landing from "./components/Landing/Landing";
 import Team from "./components/Team/Team";
 import Map from "./components/Map/Map";
-<<<<<<< HEAD
 import Events from "./components/Events/Events";
-
-=======
 import Footer from "./components/Footer/Footer";
->>>>>>> upstream/main
 
 function App() {
   return (
