329fcf782c30fff808e05e18db45331fade7982c
riteshsoni123 <comedybuddy1@gmail.com>
2021-08-06 13:40:40 +0530

    added teams page and styled it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About/About";
 import Landing from "./components/Landing/Landing";
 import Team from "./components/Team/Team";
 import Map from "./components/Map/Map";
+import Events from "./components/Event/Event";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navi/>
         <Landing/>
         <About/>
+        <Events/>
         <Team/>
         <Map/>
     </div>
