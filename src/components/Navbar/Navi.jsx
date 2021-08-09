781e0ea551f3f9d70045a6e537284eb8bff0df22
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-08-09 10:06:38 -0600

    Fix the navbar #40 issue complete
    Updated navbar with Toggle & Collapse components
    
    Navbar button added to Navbar.Collapse Component
    
    Navbar now collapses to hamburger menu for mobile

--- src/components/Navbar/Navi.jsx
+++ src/components/Navbar/Navi.jsx
@@ -1,28 +1,27 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import "./Navi.scss";
 import logo from "./logo.webp";
 
 const Navi = () => {
     return (
-        <Navbar  expand="lg" className="nav-size nav-color">
-            <Container>
-                <Navbar.Brand href="#home"><img className="animate-logo logo-size " src={logo} alt="logo" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto block">
-                        <Nav.Link href="/" className="Nav_text nav_container">Home</Nav.Link>
-                        <Nav.Link href="/team" className="Nav_text nav_container">Team</Nav.Link>
-                        <Nav.Link href="/events" className="Nav_text nav_container">Events</Nav.Link>
-                        <Nav.Link href="/gallery" className="Nav_text  nav_container">Gallery</Nav.Link>
-                        <Nav.Link href="/events" className="Nav_text nav_container">Events</Nav.Link>
-                        <Nav.Link href="/new" className="Nav_text nav_container">What's New</Nav.Link>
-                        <Nav.Link href="/new" ><button type="button" class="btn btn-danger btn-size">Sign In</button></Nav.Link>
-
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Navbar expand='lg' className="nav">
+        <Navbar.Brand href='/'><img className='nav__logo nav__logo-animate' src={logo} alt='logo'/></Navbar.Brand>   
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='nav__links'>
+                <Nav.Link href='/' className='nav__links-text'>Home</Nav.Link>
+                <Nav.Link href='/team' className='nav__links-text'>Team</Nav.Link>
+                <Nav.Link href='/events' className='nav__links-text'>Events</Nav.Link>
+                <Nav.Link href='/gallery' className='nav__links-text'>Gallery</Nav.Link>
+                <Nav.Link href='/whatsNew' className='nav__links-text'>What's New</Nav.Link>
+            </Nav>
+            <button type='button' class='nav__btn'>Sign In</button>
+        </Navbar.Collapse> 
+        
+    </Navbar> 
     )
 }
 export default Navi;
+
+
